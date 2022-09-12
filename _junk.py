242fda46a57e7e76d26bce83823722da7d852fe2
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-12 21:38:07 +0300

    download queue added

--- _junk.py
+++ _junk.py
@@ -1,7 +1,7 @@
 from utils.packager import depends
 from utils.packager import download
 
-ret = download.isInstalled('btop')
 
-# ret = depends.find_dep(path=f"/home/berkay/.yaybuntu/btop")
+depeds_= depends.find_dep(path=f"/home/berkay/.yaybuntu/btop")
+ret = download.download(depeds_)
 print(ret)
