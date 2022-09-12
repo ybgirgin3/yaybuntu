6194bab87f246bd5a800fb054d9a78946c43fb92
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-12 21:41:37 +0300

    download queue added

--- _junk.py
+++ _junk.py
@@ -3,6 +3,6 @@ from utils.packager import download
 import os
 
 
-depeds_= depends.find_dep(path=f"{os.path.expanduser('~')}/.yaybuntu/btop")
-ret = download.download(depeds_)
+deps, makedeps = depends.find_dep(path=f"{os.path.expanduser('~')}/.yaybuntu/btop")
+ret = download.download(deps, makedeps)
 print(ret)
