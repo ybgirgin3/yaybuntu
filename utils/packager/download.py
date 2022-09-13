6ad4f34ff9b7666d873d8b29576e628b31b60472
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-13 08:39:14 +0300

    isinstalled control re-configured

--- utils/packager/download.py
+++ utils/packager/download.py
@@ -8,25 +8,23 @@ ubuntu_download_command = "sudo apt install"
 def isInstalled(pkg: str, installation_folder: str = '/usr/bin') -> list:
   q = []
   for installed_apps in os.listdir(installation_folder):
-    if pkg in installed_apps:
+    if pkg == installed_apps:
       logger(f"{pkg} package already installed.. passing..")
     else:
+      logger(f"{pkg} added to installation queue..", 'warning')
       q.append(pkg)
 
-  if len(q):
-    for i in q:
-      logger(f"{i} added to installation queue..", 'warning')
-  else: logger("no package is in installation queue")
   return q
-  
 
 
 def download(deps: list, makedeps: list) -> list or bool:
   "download deps"
 
   def _process(pkg):
-    p_codes = []
-    if len(isInstalled(pkg)) > 0:
+    p_codes: list  = []  # status codes for terminal command
+    installation_q: list = isInstalled(pkg)
+
+    if len(installation_q) > 0:
       cmd = f"{ubuntu_download_command} {pkg}"
       logger(f"Running command: {cmd}")
       # TODO: still not sure about how to install packages using chrome
