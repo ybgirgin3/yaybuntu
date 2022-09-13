142fecf43aa0d34bfc418f5ec0ed14a491c0f44d
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-13 09:42:04 +0300

    download process done

--- utils/packager/download.py
+++ utils/packager/download.py
@@ -1,8 +1,15 @@
 import os
+import subprocess
 from utils.logger import logger
 from utils.extra import is_installed
 from utils.config import args
 
+ubnt_install = args()["install"]          # install command
+
+
+def _command_run(pkg):
+  subprocess.Popen(["sudo", "apt", "install", f"{pkg}"])
+  
 
 
   
@@ -11,29 +18,22 @@ def download(deps: list, mkdeps: list):
   "download deps"
   dep_installation_q = []
   make_dep_installation_q = []
-
-  
   
 
   def _download_proc(deps, mkdeps):
     deps = deps if len(deps) else None        # define none if len is 0
     mkdeps = mkdeps if len(mkdeps) else None  # define none if len is 0
-    ubnt_install = args()["install"]          # install command
+
 
     # * install deps
     if deps is not None:
       for dep in deps:
-        cur_cmd = f"{ubnt_install} {dep}"     # enter command and what for finish
-        os.system(cur_cmd)
+        _command_run(dep)
 
 
     if mkdeps is not None:
       for mk in mkdeps:
-        cur_cmd = f"{ubnt_install} {mk}"      # enter command and what for finish
-        os.system(cur_cmd)
-
-
-
+        _command_run(dep)
 
   for dep in deps:
     isExists = is_installed.is_installed(dep)  # control if pkg is installed
