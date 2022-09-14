9b98d24d941035517ce291032e4c44868057c0a6
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-14 21:20:01 +0300

    subprocess added

--- utils/packager/install.py
+++ utils/packager/install.py
@@ -3,6 +3,7 @@ import os
 
 def install(package_path: str = '/path/to/package'):
     pkgfile_path = os.path.join(package_path, 'PKGBUILD')
+    print("pkgfile path: ", pkgfile_path)
     command = f"makedeb {pkgfile_path}"
     # print("install here", package_path)
     subprocess.run(command.split())
