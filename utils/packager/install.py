41633e78895e48f1c13243bf0d1fd545aab94fa5
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-14 21:24:21 +0300

    path issue fixing

--- utils/packager/install.py
+++ utils/packager/install.py
@@ -2,9 +2,11 @@ import subprocess
 import os
 
 def install(package_path: str = '/path/to/package'):
-    pkgfile_path = os.path.join(package_path, 'PKGBUILD')
-    print("pkgfile path: ", pkgfile_path)
-    command = f"makedeb -i {pkgfile_path}"
+    # pkgfile_path = os.path.join(package_path, 'PKGBUILD')
+
+    command = f"makedeb -i PKGBUILD"
     # print("install here", package_path)
+
+    os.chdir(f'{package_path}')
     subprocess.run(command.split())
 
