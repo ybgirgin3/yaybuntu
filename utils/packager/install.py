f8f08381a23449164f9e1ba1b9d752cae5caa050
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-14 21:21:26 +0300

    makedeb command added

--- utils/packager/install.py
+++ utils/packager/install.py
@@ -4,7 +4,7 @@ import os
 def install(package_path: str = '/path/to/package'):
     pkgfile_path = os.path.join(package_path, 'PKGBUILD')
     print("pkgfile path: ", pkgfile_path)
-    command = f"makedeb {pkgfile_path}"
+    command = f"makedeb -i {pkgfile_path}"
     # print("install here", package_path)
     subprocess.run(command.split())
 
